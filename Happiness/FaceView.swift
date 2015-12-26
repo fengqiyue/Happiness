19587fff403167e82755f3c6c531bab86046a392
fengqiyue <mynameiszhangxu@gmail.com>
2015-12-26 20:32:46 +0800

    首次提交

---
+++ Happiness/FaceView.swift
@@ -0,0 +1,13 @@
+//
+//  FaceView.swift
+//  Happiness
+//
+//  Created by 张旭 on 15/12/24.
+//  Copyright © 2015年 cheri. All rights reserved.
+//
+
+import Cocoa
+
+class FaceView: UIView {
+
+}
---
+++ Happiness/HappinessViewController.swift
@@ -0,0 +1,13 @@
+//
+//  HappinessViewController.swift
+//  Happiness
+//
+//  Created by 张旭 on 15/12/24.
+//  Copyright © 2015年 cheri. All rights reserved.
+//
+
+import Cocoa
+
+class HappinessViewController: UIViewController {
+
+}
