879dc2f29f2ca6b1eacf23562b560b7a2ec449db
fengqiyue <mynameiszhangxu@gmail.com>
2016-01-05 19:45:56 +0800

    完成

--- Happiness/FaceView.swift
+++ Happiness/FaceView.swift
@@ -6,8 +6,103 @@
 //  Copyright © 2015年 cheri. All rights reserved.
 //
 
-import Cocoa
+import UIKit
 
+protocol FaceViewDataSource:class {   //作为数据源的代理
+    func smilinessForFaceView(sender:FaceView) -> Double?
+}
+
+@IBDesignable 
 class FaceView: UIView {
+    @IBInspectable
+    var linewidth:CGFloat = 3 {didSet {setNeedsDisplay()}} //线宽被改变时，重新绘制
+    @IBInspectable 
+    var scale:CGFloat = 0.90
+    @IBInspectable
+    var color:UIColor = UIColor.blueColor() {didSet {setNeedsDisplay()}}
+    
+    var faceCenter:CGPoint {
+        return convertPoint(center, fromView: superview)
+    }
+    var faceRadius:CGFloat {
+        return min(bounds.size.width, bounds.size.height) / 2 * scale
+    }
+    
+    weak var dataSource:FaceViewDataSource?  //dataSource 指向的任何对象都不会被一直存在内存里
+    
+    func scale(gesture:UIPinchGestureRecognizer) {       //缩放手势
+        if gesture.state == .Changed {
+            scale *= gesture.scale
+            gesture.scale = 1
+            self.setNeedsDisplay()
+        }
+    }
 
+    override func drawRect(rect: CGRect) {
+        let facePath = UIBezierPath(arcCenter: faceCenter, radius: faceRadius, startAngle: 0, endAngle: CGFloat(2 * M_PI), clockwise: true)
+        facePath.lineWidth = linewidth
+        color.set()
+        facePath.stroke()
+        
+        bezierPathForEye(.Left).stroke()
+        bezierPathForEye(.Right).stroke()
+        
+        print("dataSource?.smilinessForFaceView(self) = \(dataSource?.smilinessForFaceView(self))")
+        
+        let smiliness = dataSource?.smilinessForFaceView(self) ?? 0.0 //若左边为 nil， 则用右边的值
+        
+        let smilePath = bezierPathForSmile(smiliness)
+        smilePath.stroke()
+    }
+    
+    private struct Scaling {
+        static let FaceRadiusToEyeRadiusRatio: CGFloat = 10
+        static let FaceRadiusToEyeOffsetRatio: CGFloat = 3
+        static let FaceRadiusToEyeSeparationRatio: CGFloat = 1.5
+        static let FaceRadiusToMounthWidthRatio: CGFloat = 1
+        static let FaceRadiusToMounthHeightRatio: CGFloat = 3
+        static let FaceRadiusToMounthOffsetRatio: CGFloat = 3
+    }
+    
+    private enum Eye { case Left, Right }
+    
+    private func bezierPathForEye(whichEye:Eye) -> UIBezierPath
+    {
+        let eyeRadius = faceRadius / Scaling.FaceRadiusToEyeRadiusRatio
+        let eyeVerticalOffset = faceRadius / Scaling.FaceRadiusToEyeOffsetRatio
+        let eyeHorizontalSeparation = faceRadius / Scaling.FaceRadiusToEyeSeparationRatio
+        
+        var eyeCenter = faceCenter
+        eyeCenter.y -= eyeVerticalOffset
+        switch whichEye {
+        case .Left: eyeCenter.x -= eyeHorizontalSeparation / 2
+        case .Right: eyeCenter.x += eyeHorizontalSeparation / 2
+        }
+
+        
+        let path = UIBezierPath(arcCenter: eyeCenter, radius: eyeRadius, startAngle: 0, endAngle: CGFloat(2*M_PI), clockwise: true)
+        path.lineWidth = linewidth
+        return path
+    }
+    
+    private func bezierPathForSmile(fractionOfMaxSmile:Double) -> UIBezierPath {
+        
+        let mouthWidth = faceRadius / Scaling.FaceRadiusToMounthWidthRatio
+        let mouthHeight = faceRadius / Scaling.FaceRadiusToMounthHeightRatio
+        let mouthVerticalOffset = faceRadius / Scaling.FaceRadiusToMounthOffsetRatio
+        
+        let smileHeight = CGFloat(max(min(fractionOfMaxSmile, 1), -1)) * mouthHeight
+
+        
+        let start = CGPoint(x:faceCenter.x - mouthWidth / 2, y:faceCenter.y + mouthVerticalOffset)
+        let end = CGPoint(x: start.x + mouthWidth, y: start.y)
+        let cp1 = CGPoint(x: start.x + mouthWidth / 3, y: start.y + smileHeight)
+        let cp2 = CGPoint(x: end.x - mouthWidth / 3, y: cp1.y)
+
+        
+        let path = UIBezierPath()
+        path.moveToPoint(start)
+        path.addCurveToPoint(end, controlPoint1: cp1, controlPoint2: cp2)
+        return path
+    }
 }
--- Happiness/HappinessViewController.swift
+++ Happiness/HappinessViewController.swift
@@ -6,8 +6,57 @@
 //  Copyright © 2015年 cheri. All rights reserved.
 //
 
-import Cocoa
+import UIKit
 
-class HappinessViewController: UIViewController {
+
+
+class HappinessViewController: UIViewController, FaceViewDataSource {
+      
+    @IBOutlet weak var faceView: FaceView! {
+        didSet{
+            faceView.dataSource = self
+            faceView.addGestureRecognizer(UIPinchGestureRecognizer(target: faceView, action: "scale:"))
+            faceView.addGestureRecognizer(UIPanGestureRecognizer(target: self, action: "changeHappiness:"))
+        }
+    }
+    
+    var happiness:Int = 50 { // 0 = very sad, 100 = very happy
+        didSet{
+            happiness = min(max(happiness,0), 100)
+            updateUI()
+        }
+    }
+    
+    private struct Constants {
+        static let HappinessGestureScale:CGFloat = 4
+    }
+    
+    @IBAction func changeHappiness(gesture: UIPanGestureRecognizer) {
+        switch gesture.state{
+        case .Ended:fallthrough
+        case .Changed:
+            let translation = gesture.translationInView(faceView)
+            let happinessChange = -Int(translation.y / Constants.HappinessGestureScale)
+            if happinessChange != 0 {
+                happiness += happinessChange
+                gesture.setTranslation(CGPointZero, inView: faceView)  //重置以保证是增量变化
+            }
+        default:break
+        }
+    }
+    
+    @IBAction func valueChanged(sender: UISlider) {   //一个拖拽条控制微笑的弧度
+        self.happiness = Int(sender.value)
+    }
+    
+    private func updateUI() {
+        faceView.setNeedsDisplay()
+    }
+    
+    func smilinessForFaceView(sender: FaceView) -> Double? {
+        return Double(happiness-50)/50.0
+    }
+    
+    
 
 }
